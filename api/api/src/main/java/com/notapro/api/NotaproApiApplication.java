1e5420a90e02973f81fb1eaf66e8785bc0f50572
RomeuL <pessoal@romeu.dev.br>
2025-05-03 16:00:11 -0300

    refatoração do projeto inicial para usar java 21

---
+++ api/api/src/main/java/com/notapro/api/NotaproApiApplication.java
@@ -0,0 +1,13 @@
+package com.notapro.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotaproApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotaproApiApplication.class, args);
+	}
+
+}
---
+++ api/api/src/test/java/com/notapro/api/NotaproApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notapro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaproApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
